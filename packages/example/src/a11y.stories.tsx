9ee41f131ff37d2a4cc6404081ecf4ba77489be5
Yusuf YILDIRIM <yusuf@tinythin.gs>
2024-08-13 09:57:54 +0200

    chore: remove unnecessary stuff

--- packages/example/src/a11y.stories.tsx
+++ packages/example/src/a11y.stories.tsx
@@ -1,4 +1,4 @@
-// import { useLadleContext, ActionType, ThemeState, action } from "@ladle/react";
+import { useLadleContext, ActionType, ThemeState, action } from "@ladle/react";
 import type { Story } from "@ladle/react";
 
 const empty = "";
@@ -38,33 +38,33 @@ export const Responsive: Story = () => {
 Responsive.meta = {
   width: "xsmall",
 };
-//
-// export const Issues: Story = () => {
-//   const { globalState, dispatch } = useLadleContext();
-//   return (
-//     <>
-//       <input />
-//       <button
-//         style={{ backgroundColor: `red${empty}`, color: "darkRed" }}
-//         onClick={() => action("test")()}
-//       >
-//         Inaccessible button
-//       </button>
-//       <button
-//         onClick={() =>
-//           dispatch({
-//             type: ActionType.UpdateTheme,
-//             value:
-//               globalState.theme === ThemeState.Dark
-//                 ? ThemeState.Light
-//                 : ThemeState.Dark,
-//           })
-//         }
-//       >
-//         Switch theme
-//       </button>
-//       <p style={{ background: "white" }}>Theme: {globalState.theme}</p>
-//       <img />
-//     </>
-//   );
-// };
+
+export const Issues: Story = () => {
+  const { globalState, dispatch } = useLadleContext();
+  return (
+    <>
+      <input />
+      <button
+        style={{ backgroundColor: `red${empty}`, color: "darkRed" }}
+        onClick={() => action("test")()}
+      >
+        Inaccessible button
+      </button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: ActionType.UpdateTheme,
+            value:
+              globalState.theme === ThemeState.Dark
+                ? ThemeState.Light
+                : ThemeState.Dark,
+          })
+        }
+      >
+        Switch theme
+      </button>
+      <p style={{ background: "white" }}>Theme: {globalState.theme}</p>
+      <img />
+    </>
+  );
+};
